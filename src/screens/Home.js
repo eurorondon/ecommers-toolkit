b0c5fe0c23e8e09e469951d250fe7c44f8b4d861
eurorondon03 <eurorondon03@gmail.com>
2023-06-16 17:08:45 -0400

    cambiando de bibloteca de multi-carousel a slick carousel , por no poder editar las arrows  ni dots

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ShopSections from "../components/homeComponents/ShopSection/ShopSection";
 import Header from "../components/Header";
-import NewProducts from "../components/homeComponents/Newproducts";
 import Categorias from "../components/homeComponents/Categorias";
+import Carousel from "../components/homeComponents/Carousel";
 
 // import GridProductList from "../components/homeComponents/GridProductList";
 
@@ -10,8 +10,9 @@ const Home = () => {
   return (
     <div className="bg-neutral-100">
       <Header />
+      <Carousel />
       <Categorias />
-      <NewProducts />
+
       <ShopSections />
     </div>
   );
